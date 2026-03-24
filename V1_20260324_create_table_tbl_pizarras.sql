fd3527589367456322de6c9af847c5d22fc1b905
AngeLSanchez210 <is21001@ues.edu.sv>
2026-03-24 12:04:22 -0600

    correcion tabla reporte fallos

--- V1_20260324_create_table_tbl_pizarras.sql
+++ V1_20260324_create_table_tbl_pizarras.sql
@@ -2,6 +2,4 @@ CREATE TABLE tbl_pizarras (
     id_pizarra SERIAL PRIMARY KEY,
     ubicacion VARCHAR(50) NOT NULL,
     estado VARCHAR(20) CHECK (estado IN ('Operativa', 'En Reparación', 'Fuera de Servicio'))
-    id_tecnico INT,
-    id_pizarra INT
 );
--- V1_20260324_create_table_tbl_reportes_fallos.sql
+++ V1_20260324_create_table_tbl_reportes_fallos.sql
@@ -1,5 +1,7 @@
 CREATE TABLE tbl_reportes_fallos (
     id_reporte SERIAL PRIMARY KEY,
     fecha DATE DEFAULT CURRENT_DATE,
-    descripcion VARCHAR(50)
+    descripcion VARCHAR(50),
+    id_tecnico INT,
+    id_pizarra INT
 );
