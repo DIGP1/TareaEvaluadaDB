1905441e9e3136688827f31446f1a128dc1684a6
DIGP1 <diegogarci098@gmail.com>
2026-03-24 13:12:52 -0600

    update: modificacion del main para correccion de nombre de archivo

--- 20260324_main.sql
+++ 20260324_main.sql
@@ -12,6 +12,6 @@
 \i docker-entrypoint-initdb.d/V1_20260324_select_tbl_pizarras.sql
 
 -- Modulo 2: Modificacion de tabla y creacion de indices
-\i docker-entrypoint-initdb.d/V2_20260324_alter_tbl_reporte_fallos.sql
+\i docker-entrypoint-initdb.d/V2_20260324_alter_table_tbl_reportes_fallos.sql
 \i docker-entrypoint-initdb.d/V2_20260324_create_indexs_tbl_reporte_fallos.sql
 
