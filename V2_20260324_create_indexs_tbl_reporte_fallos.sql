51767a589f8ac0c44e55279d14fa6b69e9b1def1
AngeLSanchez210 <is21001@ues.edu.sv>
2026-03-24 13:48:21 -0600

    Justifaciones

--- V2_20260324_create_indexs_tbl_reporte_fallos.sql
+++ V2_20260324_create_indexs_tbl_reporte_fallos.sql
@@ -5,3 +5,8 @@ create index idx_reporte_fallos_id_tecnico on tbl_reportes_fallos (id_tecnico);
 -- rapidamente las filas que cumplen con la condicion de fecha o id_tecnico, mejorando significativamente el rendimiento de la consultas
 -- y el mejorando el uso de recursos del servidor.
 
+-- El equipamiento adicional
+-- Los indices optimizan ciertos pasos pero entre incrementos o cambios en la tabla aumentan el tamaño de los backups
+-- por ende la creacion de indices debe ser balanceada con el rendimiento y el espacio de almacenamiento disponible, ademas de considerar el volumen de datos y la frecuencia de las consultas que se realizaran sobre la tabla tbl_reportes_fallos.
+-- El administrador no debe indexar todo, sino de forma estrategia para que no sea contraproducente para el rendimiento general del sistema.
+
--- V3_20260324_create_maintenance.sql
+++ V3_20260324_create_maintenance.sql
@@ -1,5 +1,25 @@
 VACUUM ANALYZE tbl_reportes_fallos;
 
+
+
+--JUSTIFICACION
+
+-- se ha implementado la consuta VACUUM ANALYZE para mantener la eficiencia y el rendimiento de la base de datos a medida que se realizan inserciones
+--mientras que vacuum limpia el rastro de informacion restante de tuplas muertas, analyze actualiza las estadísticas de la tabla, lo que permite al planificador de consultas
+--tomar decisiones informadas sobre cómo ejecutar las consultas de manera eficiente.
+
+--En conclusion la combinancion es excelente en el impacto positivo en el rendimiento de la base de datos,
+--por medio de reducir eñ trabajo del hardware a leer todos los datos.
+--Y la toma de decisiones del planificador de consultas y analizar si es mas eficiente usar indices secundarios o escaneo secuencial.
+
+
+Se implementa 'VACUUM ANALYZE' de forma conjunta para garantizar la salud integral 
+del sistema. Mientras VACUUM limpia el rastro físico de la fragmentación 
+(evitando el crecimiento del disco), ANALYZE proporciona los metadatos necesarios 
+para que el Planificador de Consultas aproveche los índices de fecha y técnico 
+creados anteriormente.
+*/
+
 DO $$
 BEGIN
     RAISE NOTICE 'Mantenimiento Realizado.';
