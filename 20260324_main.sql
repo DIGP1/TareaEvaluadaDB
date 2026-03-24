0d9607f95a3e06bcc22119239ab067e2bc1a211c
DIGP1 <diegogarci098@gmail.com>
2026-03-24 13:06:50 -0600

    Integracion de modulo 2 a main

--- 20260324_main.sql
+++ 20260324_main.sql
@@ -1,3 +1,4 @@
+--Modulo 1: Creacion de tablas, funciones, triggers e insercion de datos
 \i docker-entrypoint-initdb.d/V1_20260324_create_table_tbl_pizarras.sql
 \i docker-entrypoint-initdb.d/V1_20260324_create_table_tbl_tecnicos.sql
 \i docker-entrypoint-initdb.d/V1_20260324_create_table_tbl_reportes_fallos.sql
@@ -9,3 +10,8 @@
 \i docker-entrypoint-initdb.d/V1_20260324_insert_tbl_reportes_fallos.sql
 \i docker-entrypoint-initdb.d/V1_20260324_select_tbl_tbl_reportes_fallos.sql
 \i docker-entrypoint-initdb.d/V1_20260324_select_tbl_pizarras.sql
+
+-- Modulo 2: Modificacion de tabla y creacion de indices
+\i docker-entrypoint-initdb.d/V2_20260324_alter_tbl_reporte_fallos.sql
+\i docker-entrypoint-initdb.d/V2_20260324_create_indexs_tbl_reporte_fallos.sql
+
