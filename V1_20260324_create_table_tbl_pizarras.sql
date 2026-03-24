5182587ea6a95f6bdc38b7273ea39d786535749f
AngeLSanchez210 <is21001@ues.edu.sv>
2026-03-24 12:03:10 -0600

    correccion tabla pizarras

--- V1_20260324_create_table_tbl_pizarras.sql
+++ V1_20260324_create_table_tbl_pizarras.sql
@@ -2,4 +2,6 @@ CREATE TABLE tbl_pizarras (
     id_pizarra SERIAL PRIMARY KEY,
     ubicacion VARCHAR(50) NOT NULL,
     estado VARCHAR(20) CHECK (estado IN ('Operativa', 'En Reparación', 'Fuera de Servicio'))
+    id_tecnico INT,
+    id_pizarra INT
 );
